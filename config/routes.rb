56efcbf4829809b6f4337b6720173776d1111a89
Hanane Merroun <hananmerroune@gmail.com>
2021-12-30 00:43:14 +0100

    delete views devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :employes
+ Rails.application.routes.draw do
+
+  devise_for :employes, controllers:{
+    sessions: 'employes/sessions',
+    registrations: 'employes/registrations'
+  }
    devise_for :recruteurs, controllers: {
         sessions: 'recruteurs/sessions',
-        sessions: 'employes/sessions'
+         registrations: 'recruteurs/registrations'
       }
 
+ 
   root 'home#index'
      devise_scope :recruteur do  
      get '/recruteurs/sign_out' => 'devise/sessions#destroy'     
