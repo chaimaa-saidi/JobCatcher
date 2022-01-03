dcb9e9c8575cd15f1305ad6e1123460f63a1a734
hafssa-rabah <84786387+hafssa-rabah@users.noreply.github.com>
2022-01-03 09:51:34 +0100

    Update registrations_controller.rb

--- app/controllers/employes/registrations_controller.rb
+++ app/controllers/employes/registrations_controller.rb
@@ -3,7 +3,13 @@
 class Employes::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+     before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+devise_parameter_sanitizer.permit(:account_update, keys: [:name,:cin, :tel, :naissance, :sexe, :avatar, :lettre, :images])
+end
   # GET /resource/sign_up
   # def new
   #   super
