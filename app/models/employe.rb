ae1bd2e37b69c53a3fd93fff3b7a55d97035f0a0
hafssa-rabah <84786387+hafssa-rabah@users.noreply.github.com>
2022-01-03 10:02:50 +0100

    Update employe.rb
    L'ajout de type de sexe (enum)
    (has_one_attached ) : chaque employé a un seule photo(avatar) de profil et un seule CV(images)

--- app/models/employe.rb
+++ app/models/employe.rb
@@ -3,4 +3,7 @@ class Employe < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum sexe: [ :Homme, :Femme]
+  has_one_attached :avatar
+  has_one_attached :images
 end
